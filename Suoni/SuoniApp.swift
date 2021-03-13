7e49a24e5f696ac2e9b17da90cf8c4eac9de9b00
Elechim01 <mikymanny015@gmail.com>
2021-03-13 18:42:52 +0100

    Initial Commit

---
+++ Suoni/SuoniApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuoniApp.swift
+//  Suoni
+//
+//  Created by Michele Manniello on 13/03/21.
+//
+
+import SwiftUI
+
+@main
+struct SuoniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
